df09d1f26eb646f8f00f6ef342f3d5598983ddff
Kwanit Gupta <gupta.45@iitj.ac.in>
2023-08-06 16:15:34 +0530

    Delete B19EE046_Q3_A4.py

--- B19EE046_Q3_A4.py
+++
@@ -1,3 +0,0 @@
-# Note:- Question-3 is done on Colab Notebook mostly because it involved shell commands, 
-# rather than python commands. Still for the sake of submission, I create note over here
-# Kindly check on Colab Notebook attached within zip
